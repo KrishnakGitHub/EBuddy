72db4f1da07c7a41bbf4e7d23244c194fe7a9b83
Krishna <krishnakjee2016@gmail.com>
2026-04-22 01:23:29 +0530

    ui init

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Welcome to E Buddy",
+  description: "E Buddy is an AI-powered assistant designed to help you with your tasks and provide information on a wide range of topics. Whether you need help with work, school, or just want to have a conversation, E Buddy is here to assist you. With its advanced natural language processing capabilities, E Buddy can understand and respond to your queries in a human-like manner. Try it out and see how E Buddy can make your life easier!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
